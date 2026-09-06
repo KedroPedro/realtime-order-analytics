d20bea4d12b9743f891596d485e85080c1c03db4
KedroPedro <zyazyulyam727@gmail.com>
2026-09-06 21:09:18 +0300

    define order entity

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KedroPedro/realtime-order-analytics
+
+go 1.27.0
---
+++ internal/domain/entity/order.go
@@ -0,0 +1,29 @@
+package entity
+
+import (
+	"time"
+	"uuid"
+)
+
+type Order struct {
+	Id        uuid.UUID
+	OwnerId   uuid.UUID
+	CreatedAt time.Time
+	Items     []OrderItem
+	Address   *Address
+	Status    string
+	Total     int64
+}
+
+type OrderItem struct {
+	Id       uuid.UUID
+	Name     string
+	Quantity int64
+	Price    int64
+}
+
+type Address struct {
+	Country string
+	City    string
+	ZIP     string
+}
