7858b55faa8c54e61c7ed54219f610eddf490a7e
KedroPedro <zyazyulyam727@gmail.com>
2026-09-09 21:59:12 +0300

    add usecase for outbox worker

---
+++ internal/application/usecases/read_and_publish_order.go
@@ -0,0 +1,39 @@
+package usecases
+
+import (
+	"context"
+
+	"github.com/KedroPedro/realtime-order-analytics/internal/domain/interfaces"
+)
+
+type ReadAndPublishOrderUsecase struct {
+	outbox    interfaces.OrdersOutbox
+	publisher interfaces.EventPublisher
+}
+
+func newReadAndPublishOrderUsecase(
+	outbox interfaces.OrdersOutbox,
+	publisher interfaces.EventPublisher,
+) *ReadAndPublishOrderUsecase {
+	return &ReadAndPublishOrderUsecase{
+		outbox:    outbox,
+		publisher: publisher,
+	}
+}
+
+func (uc *ReadAndPublishOrderUsecase) Execute(ctx context.Context) error {
+	events, err := uc.outbox.GetEventsBatch(ctx)
+	if err != nil {
+		return err
+	}
+
+	if err := uc.publisher.PublishBatch(ctx, events); err != nil {
+		return err
+	}
+
+	if err := uc.outbox.PublishEvents(ctx, events); err != nil {
+		return err
+	}
+
+	return nil
+}
